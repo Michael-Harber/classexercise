67b174732fa0ef81cfb2d26fa590d3511bdd2db7
Michael Harber <noreply>
2023-12-05 16:02:48 -0800

    fixed

--- auth.sh
+++ auth.sh
@@ -1,7 +1,31 @@
 # new ssh sessions
-cat auth.log | egrep sshd.*opened
-echo $?
+# cat auth.log | egrep sshd.*opened
+# echo $?
 
 # additional test for auth.log
 # cat auth.log | egrep sshd.*closed
 # echo $?
+
+
+PATTERN=sshd.*opened
+
+
+
+
+egrep "$PATTERN" should-match-auth.txt
+
+
+
+failure=0
+if egrep -v "$PATTERN" should-match-auth.txt
+then
+echo "failure"
+failure=1
+else
+echo "success on should match"
+fi
+
+echo $?
+
+exit $failure
+
